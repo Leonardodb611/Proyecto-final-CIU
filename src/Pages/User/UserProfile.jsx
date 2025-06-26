4bb3ecb2d06a44628f8eced85f30510da34595be
brendalera <brendalera17@gmail.com>
2025-06-26 00:39:52 -0300

    agregando alineación del banner

--- src/Pages/User/UserProfile.jsx
+++ src/Pages/User/UserProfile.jsx
@@ -9,28 +9,29 @@ function Profile() {
   const { publications, setPublications } = useGetPublications();
   const { randomUser } = useRandomUser();
 
-  if (!publications || publications.length === 0 || !randomUser) {
-    return (
-      <p style={{ color: "white", fontWeight: '600' }} className="cargando">
-        No se pudo cargar el perfil
-      </p>
-    );
-  }
-  const publicationsByUser = randomUser
+  const isLoading = !publications || !randomUser;
+
+  const publicationsByUser = !isLoading
     ? publications.filter((p) => p.UserId === randomUser.id)
     : [];
 
   return (
     <main className="container-main">
-      <UserBanner user={randomUser}></UserBanner>
-      {publicationsByUser.map((p) => (
-        <CardFeed
-          publication={p}
-          key={p.id}
-          updateUsers={setPublications}
-          publications={publications}
-        />
-      ))}
+      <UserBanner user={randomUser} />
+      {isLoading ? (
+        <p style={{ color: "white", fontWeight: "600" }} className="cargando">
+          Cargando perfil...
+        </p>
+      ) : (
+        publicationsByUser.map((p) => (
+          <CardFeed
+            publication={p}
+            key={p.id}
+            updateUsers={setPublications}
+            publications={publications}
+          />
+        ))
+      )}
     </main>
   );
 }
--- src/components/User_banner/UserBanner.jsx
+++ src/components/User_banner/UserBanner.jsx
@@ -1,20 +1,27 @@
-import './UserBanner.css'
+import "./UserBanner.css";
 
 export function UserBanner({ user }) {
-
+  if (!user) {
     return (
       <div className="container-banner_profile">
-        <div className="container-img_profile">
-          <div className="img">
-            <img
-              src="https://s3.ppllstatics.com/lasprovincias/www/pre2017/multimedia/RC/201501/12/media/cortadas/avatar--235x235.jpg"
-              alt=""
-            />
-          </div>
-          <div className="banner-nickName">
-                <h1>{user.nickName}</h1>
-          </div>
-        </div>
+        <h1>Cargando...</h1>
       </div>
     );
+  }
+
+  return (
+    <div className="container-banner_profile">
+      <div className="container-img_profile">
+        <div className="img">
+          <img
+            src="https://s3.ppllstatics.com/lasprovincias/www/pre2017/multimedia/RC/201501/12/media/cortadas/avatar--235x235.jpg"
+            alt=""
+          />
+        </div>
+        <div className="banner-nickName">
+          <h1>{user.nickName}</h1>
+        </div>
+      </div>
+    </div>
+  );
 }
