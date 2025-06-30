55b0c5da83c2c1270125bd1df4a4ce271588d9a7
Alvaro <bravoalva19@gmail.com>
2025-06-30 01:35:15 -0300

    Modificando login y register

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,98 +1,109 @@
-import React, { useState, useContext } from 'react'
-import { useGetUsers } from '../../hooks/useGetUsers'
+import React, { useState, useContext } from 'react';
+import { useGetUsers } from '../../hooks/useGetUsers';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { ToastContainer, toast } from 'react-toastify';
 import { Link, useNavigate } from 'react-router-dom';
 import { AuthContext } from '../../context/AuthContext';
 import { useRedirectHome } from '../../hooks/useRedirect';
-import { cambiarTitulo } from '../../utils/util'
+import { cambiarTitulo } from '../../utils/util';
 function Login() {
-    cambiarTitulo('Iniciar sesión')
-    useRedirectHome()
-    const [nickName, setNickName] = useState('');
-    const [password, setPassword] = useState('');
-    const navigate = useNavigate();
-    const { users } = useGetUsers();
-    const { login } = useContext(AuthContext);
+  cambiarTitulo('Iniciar sesión');
+  useRedirectHome();
+  const [nickName, setNickName] = useState('');
+  const [password, setPassword] = useState('');
+  const navigate = useNavigate();
+  const { users } = useGetUsers();
+  const { login } = useContext(AuthContext);
 
+  const PASSWORD_PREDETERMINADA = '123456';
 
-    const PASSWORD_PREDETERMINADA = '123456'; 
+  function notifyError() {
+    toast.error('Credenciales incorrectas');
+  }
 
-    function notifyError() {
-        toast.error('Credenciales incorrectas');
+  function notifyOk() {
+    toast.success('Inicio de sesión exitoso');
+  }
+
+  function handleLogin() {
+    if (!nickName || !password) {
+      toast.error('Por favor, completa todos los campos');
+      return;
     }
 
-    function notifyOk() {
-        toast.success('Inicio de sesión exitoso');
+    const userExists = users.find(
+      (user) => user.nickName.toLowerCase() === nickName.toLowerCase()
+    );
+
+    if (userExists && password === PASSWORD_PREDETERMINADA) {
+      notifyOk();
+      login(userExists);
+      console.log(
+        'Login hecho, revisá localStorage:',
+        localStorage.getItem('usuario')
+      );
+      setTimeout(() => {
+        navigate('/home');
+      }, 2000);
+    } else {
+      notifyError();
     }
+  }
 
-    function handleLogin() {
-        if (!nickName || !password) {
-            toast.error('Por favor, completa todos los campos');
-            return;
-        }
-
-        const userExists = users.find(
-            user => user.nickName.toLowerCase() === nickName.toLowerCase()
-        );
-
-        if (userExists && password === PASSWORD_PREDETERMINADA) {
-        notifyOk();
-        login(userExists);
-        console.log('Login hecho, revisá localStorage:', localStorage.getItem('usuario'));
-        setTimeout(() => {
-            navigate('/home');
-        }, 2000);
-        }   else {
-            notifyError();
-        }
-
-    }
-
-    
-
-
-
-
-    return (
-    <div className='w-100 bg-dark d-flex justify-content-center align-items-center' >  
-        <form className="bg-white d-flex justify-content-center align-items-center flex-column w-50 h-90 rounded">
+  return (
+    <div className='w-100 bg-dark d-flex justify-content-center align-items-center'>
+      <form className='bg-white d-flex justify-content-center align-items-center flex-column w-50 h-90 rounded'>
         <h3 className='mt-4'>Login</h3>
-        <div className='p-5 '>
-        
-        <div data-mdb-input-init className="form-outline mb-4">
-            <input type="string" id="nickName" className="form-control" required onChange={(e)=>setNickName(e.target.value)}/>
-            <label className="form-label" for="nickName">Nombre de usuario</label>
+        <div className='p-5'>
+          <div data-mdb-input-init className='form-outline mb-4'>
+            <input
+              type='text'
+              id='nickName'
+              className='form-control'
+              required
+              onChange={(e) => setNickName(e.target.value)}
+            />
+            <label className='form-label' htmlFor='nickName'>
+              Nombre de usuario
+            </label>
+          </div>
+
+          <div data-mdb-input-init className='form-outline mb-4'>
+            <input
+              type='password'
+              id='contraseña'
+              className='form-control'
+              onChange={(e) => setPassword(e.target.value)}
+              required
+            />
+            <label className='form-label' htmlFor='contraseña'>
+              Contraseña
+            </label>
+          </div>
+
+          <div className='row mb-4 w-100'>
+            <div className='col d-flex justify-content-center w-100'>
+              <button
+                data-mdb-ripple-init
+                type='button'
+                className='btn btn-primary btn-block mb-4 w-100 w-md-50'
+                onClick={() => handleLogin()}
+              >
+                Iniciar sesión
+              </button>
+            </div>
+          </div>
+
+          <div className='text-center'>
+            <p>
+              No tenes cuenta? <Link to='/register'>Registrarse</Link>
+            </p>
+          </div>
         </div>
-
-    
-    <div data-mdb-input-init className="form-outline mb-4">
-        <input  type="password" id="contraseña" className="form-control" onChange={(e)=>setPassword(e.target.value)} required/>
-        <label className="form-label" for="contraseña">Contraseña</label>
+      </form>
+      <ToastContainer />
     </div>
-
-    
-    <div className="row mb-4 w-100">
-        <div className="col d-flex justify-content-center w-100">
-        <button data-mdb-ripple-init type="button" className="btn btn-primary btn-block mb-4 w-100 w-md-50 " onClick={()=> handleLogin() }>Iniciar sesión</button>
-        </div>
-
-        
-    </div>
-
-    
-    
-    
-    <div className="text-center">
-        <p>No tenes cuenta? <Link to="/register">Registrarse</Link></p>
-        
-    </div>
-    </div>
-    </form>
-        <ToastContainer/>
-        </div>
-        
-    )
+  );
 }
 
-export default Login
+export default Login;
--- src/Pages/Register/Register.jsx
+++ src/Pages/Register/Register.jsx
@@ -1,123 +1,129 @@
-import React, { useState } from 'react'
-import { useGetUsers } from '../../hooks/useGetUsers'
+import React, { useState } from 'react';
+import { useGetUsers } from '../../hooks/useGetUsers';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { ToastContainer, toast } from 'react-toastify';
 import { Link, useNavigate } from 'react-router-dom';
 import { useRedirectHome } from '../../hooks/useRedirect';
-import { cambiarTitulo } from '../../utils/util'
+import { cambiarTitulo } from '../../utils/util';
 
 function Register() {
-  cambiarTitulo('Registrarse')
-  useRedirectHome()
-  const [nickName, setNickName] = useState('')
-  const [email, setEmail] = useState('')
-  const navigate = useNavigate()
-  const usuarios = useGetUsers()
-  const setUsuario = (e) =>{
-    
-    setNickName(e)
-  }
-  const setEmailNuevo = (e) =>{
-    
-    setEmail(e)
-    console.log(email)
-  }
+  cambiarTitulo('Registrarse');
+  useRedirectHome();
+  const [nickName, setNickName] = useState('');
+  const [email, setEmail] = useState('');
+  const navigate = useNavigate();
+  const usuarios = useGetUsers();
+  const setUsuario = (e) => {
+    setNickName(e);
+  };
+  const setEmailNuevo = (e) => {
+    setEmail(e);
+  };
 
   function validarEmail(email) {
-  const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-  return regex.test(email);
-}
+    const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return regex.test(email);
+  }
 
-  
-  function notifyError(){toast.error('Hubo un error en la solicitud');}
-  function notifyErrorUser(){toast.error('Hubo un error: nickName existente o email erroneo');}
-  function notifyOk(){toast.success('Usuario creado con exito');}
+  function notifyError() {
+    toast.error('Hubo un error en la solicitud');
+  }
+  function notifyErrorUser() {
+    toast.error('Hubo un error: nickName existente o email erroneo');
+  }
+  function notifyOk() {
+    toast.success('Usuario creado con exito');
+  }
 
-  
   async function crearUsuario(nombre, emailNuevo) {
- 
-  if(!usuarios.users.map((nombre) => nombre.nickName).includes(nombre) && validarEmail(email)){
-    try{
-      let respuesta = await fetch("http://localhost:3001/users", {
-        method: 'POST',
-        headers: {'Content-Type' : 'application/json'},
-        body: JSON.stringify({  
-          nickName : nombre,
-          email: emailNuevo
-        })
-      })
-      if(!respuesta.ok){
-        throw new Error ('error en la solicitud', respuesta)
-       
+    if (
+      !usuarios.users.map((nombre) => nombre.nickName).includes(nombre) &&
+      validarEmail(email)
+    ) {
+      try {
+        let respuesta = await fetch('http://localhost:3001/users', {
+          method: 'POST',
+          headers: { 'Content-Type': 'application/json' },
+          body: JSON.stringify({
+            nickName: nombre,
+            email: emailNuevo,
+          }),
+        });
+        if (!respuesta.ok) {
+          throw new Error('error en la solicitud', respuesta);
+        }
+
+        const data = await respuesta.json();
+        console.log('usuario creado', data);
+        notifyOk();
+        setTimeout(() => {
+          navigate('../login');
+        }, 3000);
+        setNickName('');
+        setEmail('');
+      } catch (error) {
+        console.log(error);
+        notifyError();
       }
-      
-      const data = await respuesta.json()
-      console.log("usuario creado" , data)
-      notifyOk()
-      setTimeout(()=>{
-      navigate('../login')
-      }, 3000)
-      setNickName('')
-      setEmail('')
-      
-    }
-    catch(error){
-      console.log(error)
-      notifyError()
+    } else {
+      notifyErrorUser();
     }
   }
-  else{
-    notifyErrorUser()
-  }
-    
-    
-  }
-
-  
-
-
-
 
   return (
-    <div className='w-100 bg-dark d-flex justify-content-center align-items-center' >
-      
-    <form className="bg-white d-flex justify-content-center align-items-center flex-column w-50 h-90 rounded">
-      <h3 className='mt-4'>Registro</h3>
-  <div className='p-5 '>
-    
-  <div data-mdb-input-init class="form-outline mb-4">
-    <input type="string" id="form2Example1" class="form-control" required onChange={(e)=>setUsuario(e.target.value)}/>
-    <label class="form-label" for="form2Example1">Nombre de usuario</label>
-  </div>
+    <div className='w-100 bg-dark d-flex justify-content-center align-items-center'>
+      <form className='bg-white d-flex justify-content-center align-items-center flex-column w-50 h-90 rounded'>
+        <h3 className='mt-4'>Registro</h3>
+        <div className='p-5'>
+          <div data-mdb-input-init className='form-outline mb-4'>
+            <input
+              type='text'
+              id='form2Example1'
+              className='form-control'
+              required
+              onChange={(e) => setUsuario(e.target.value)}
+            />
+            <label className='form-label' htmlFor='form2Example1'>
+              Nombre de usuario
+            </label>
+          </div>
 
- 
-  <div data-mdb-input-init class="form-outline mb-4">
-    <input  type="email" id="form2Example2" class="form-control" onChange={(e)=>setEmailNuevo(e.target.value)} required/>
-    <label class="form-label" for="form2Example2">Email</label>
-  </div>
+          <div data-mdb-input-init className='form-outline mb-4'>
+            <input
+              type='email'
+              id='form2Example2'
+              className='form-control'
+              required
+              onChange={(e) => setEmailNuevo(e.target.value)}
+            />
+            <label className='form-label' htmlFor='form2Example2'>
+              Email
+            </label>
+          </div>
 
-  
-  <div class="row mb-4 w-100">
-    <div class="col d-flex justify-content-center w-100">
-      <button data-mdb-ripple-init type="button" class="btn btn-primary btn-block mb-4 w-100 w-md-50 " onClick={()=>crearUsuario(nickName, email) }>Sign in</button>
+          <div className='row mb-4 w-100'>
+            <div className='col d-flex justify-content-center w-100'>
+              <button
+                data-mdb-ripple-init
+                type='button'
+                className='btn btn-primary btn-block mb-4 w-100 w-md-50'
+                onClick={() => crearUsuario(nickName, email)}
+              >
+                Sign in
+              </button>
+            </div>
+          </div>
+
+          <div className='text-center'>
+            <p>
+              Ya tenes cuenta? <Link to='/login'>Iniciar sesión</Link>
+            </p>
+          </div>
+        </div>
+      </form>
+      <ToastContainer />
     </div>
-
-    
-  </div>
-
-  
-  
-  
-  <div class="text-center">
-    <p>Ya tenes cuenta? <Link to="/login">Iniciar sesión</Link></p>
-    
-  </div>
-  </div>
-</form>
-    <ToastContainer/>
-    </div>
-    
-  )
+  );
 }
 
-export default Register
+export default Register;
